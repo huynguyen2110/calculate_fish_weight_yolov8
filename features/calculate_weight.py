d0120df62dc6b7520cc5d6e46b5f08d188563841
Kai <nguyenbahuy2110@gmail.com>
2023-07-20 23:32:00 +0700

    fix function calculate weight

--- features/calculate_weight.py
+++ features/calculate_weight.py
@@ -1,12 +1,16 @@
 def calculate_weight(fish):
     if fish[0] == "carp":
-        return 1
+        weight = 0.0146 * fish[1] ** 3.014
+        return round(weight, 1)
     elif fish[0] == "catfish":
-        return 10
+        weight = 0.0194 * fish[1] ** 2.971
+        return round(weight, 1)
     elif fish[0] == "tilapia":
-        return 100
+        weight = 0.0114 * fish[1] ** 2.980
+        return round(weight, 1)
     elif fish[0] == "perch":
-        return 1000
+        weight = 0.0153 * fish[1] ** 2.958
+        return round(weight, 1)
     else:
-        return "unknown"
+        return 0
 
--- features/get_skeleton.py
+++ features/get_skeleton.py
@@ -3,6 +3,11 @@ import numpy as np
 from PIL import Image
 from .get_longest_distance import get_longest_distance
 
+PIXEL2CM = 0.06
+
+def pixel_to_cm(val):
+    return PIXEL2CM * val
+
 
 def get_skeleton(img):
     images = np.array(Image.open(img))
@@ -17,4 +22,5 @@ def get_skeleton(img):
     cv2.imwrite('static/segment/fish_segment.png', thinned)
     length_object = get_longest_distance('static/segment/fish_segment.png')
 
-    return int(round(length_object.value, 0))
+    length = pixel_to_cm(int(round(length_object.value, 0)))
+    return length
